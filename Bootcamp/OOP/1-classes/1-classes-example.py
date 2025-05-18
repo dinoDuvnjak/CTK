148bd2e6f0a69a94fe0e9bd82d01158eb787050b
dinoDuvnjak <dino.duvnjak24@gmail.com>
2025-05-18 18:15:05 +0200

    poceo python

--- Bootcamp/OOP/1-classes/1-classes-example.py
+++ Bootcamp/OOP/1-classes/1-classes-example.py
@@ -1,37 +1,39 @@
 
 # Ako zelimo izracunati prosjek ocjena studenta, moramo koristiti funkciju average() koja prima listu ocjena studenta i vraca prosjek.
-# student = {'name': 'John Doe','age': 25,'grades': [80, 85, 90, 95, 100]}
+student = {'name': 'John Doe','age': 25,'grades': [80, 85, 90, 95, 100]}
 
-# def average(sequence):
-#     return sum(sequence) / len(sequence)
+#Funkicija koje ne zna da li je student ili studentica
+# ovo bi trebalo da bude funkcija u objektu student
+def average(sequence):
+    return sum(sequence) / len(sequence)
 
-# print(average(student['grades']))
+print(average(student['grades']))
 
 # Objektno orijentirano programiranje (OOP) je paradigma programiranja koja koristi objekte i klase u programiranju.
 # mozemo kreirati klasu Student koja ce sadrzavati metodu average() koja racuna prosjek ocjena studenta.
 
-# class Student:
-#     def __init__(self, name, age, grades):
-#         self.name = name
-#         self.age = age
-#         self.grades = grades
+class Student:
+    def __init__(self, name, age, grades):
+        self.name = name
+        self.age = age
+        self.grades = grades
 
-#     def __str__(self): # magicna metoda koja se poziva kada se pozove funkcija print() na objektu
-#         return f'Name: {self.name}, Age: {self.age}, Grades: {self.grades}'    
+    def __str__(self): # magicna metoda koja se poziva kada se pozove funkcija print() na objektu
+        return f'Name: {self.name}, Age: {self.age}, Grades: {self.grades}'    
     
-#     # magicna metoda koja se poziva kada se pozove funkcija repr() na objektu
-#     # koristi se za reprezentaciju objekta u obliku stringa, vecinom za debugiranje
-#     def __repr__(self): 
-#         return f"Student('{self.name}', {self.age}, {self.grades})"
+    # magicna metoda koja se poziva kada se pozove funkcija repr() na objektu
+    # koristi se za reprezentaciju objekta u obliku stringa, vecinom za debugiranje
+    def __repr__(self): 
+        return f"Student('{self.name}', {self.age}, {self.grades})"
 
-#     def average(self):
-#         return sum(self.grades) / len(self.grades)
+    def average(self):
+        return sum(self.grades) / len(self.grades)
 
 
-# student = Student('John Doe', 25, [80, 85, 90, 95, 100])
-# print(student)
-# print(repr(student))
-# print(student.average())
+student = Student('John Doe', 25, [80, 85, 90, 95, 100])
+print(student)
+print(repr(student))
+print(student.average())
 
 
 
